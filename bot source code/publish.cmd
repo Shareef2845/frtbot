9d6f047d2b9a6fc446ac3cb6997ac5ebd1a1c4a6
Shareef2845 <115469282+Shareef2845@users.noreply.github.com>
2022-10-23 12:51:17 +0530

    Add files via upload

---
+++ bot source code/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\qna-final-bot-8861-Web-Deploy.pubxml -p:Password=z5Z83rYkzePrNPt4d1WK95m2tsr0w3rloiT4yfEMbBdnDPxoopCDpvBt0fSr
+
